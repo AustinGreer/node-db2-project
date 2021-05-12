29e69cbbe4d1d8d610be72c19095f28cdfab2283
Austin Greer <agreer.ag12@gmail.com>
2021-05-12 19:44:42 -0400

    project setup is complete. Basic scaffolding for migrations are complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,9 @@
-// DO YOUR MAGIC
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('cars')
+}
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('cars')
+}
