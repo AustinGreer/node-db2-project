7c2139348b0ed4b70e80711f0c21145dc0ac56ac
Austin Greer <agreer.ag12@gmail.com>
2021-05-12 20:24:39 -0400

    happy path of get all cars request is now working as expected

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,14 @@
-// DO YOUR MAGIC
+const router = require('express').Router()
+const Cars = require('./cars-model')
+
+// get - resolves to an array of cars
+router.get('/', async (req, res) => {
+    try {
+        const currentCars = await Cars.getAll()
+        res.json(currentCars)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+module.exports = router
