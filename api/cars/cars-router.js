0701c73792397284628e2d966b5656a02447996e
Austin Greer <agreer.ag12@gmail.com>
2021-05-12 20:43:49 -0400

    built error handling middleware

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -11,4 +11,11 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
 module.exports = router
